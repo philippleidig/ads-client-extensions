52335821a08127e0ab523ee7362f3275df4417f1
philippleidig <P.Leidig@beckhoff.com>
2024-12-09 21:35:01 +0100

    chore: draft ads router and system service for testing

---
+++ test/TwinCAT.Ads.Extensions.Tests/Infrastructure/TestLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TwinCAT.Ads.Extensions.Tests.Infrastructure
+{
+    public class TestLogger : ILogger
+    {
+        private readonly TestContext _context;
+
+        public TestLogger(TestContext context)
+        {
+            _context = context;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = formatter(state, exception);
+            _context.WriteLine(message);
+        }
+    }
+}
---
+++ test/TwinCAT.Ads.Extensions.Tests/Infrastructure/TwinCAT.cs
@@ -0,0 +1,53 @@
+﻿using TwinCAT.Ads.SystemService;
+using TwinCAT.Ads.TcpRouter;
+using TwinCAT.Router;
+
+namespace TwinCAT.Ads.Extensions.Tests.Infrastructure
+{
+    [TestClass]
+    public static class GlobalTestSetup
+    {
+        private static CancellationTokenSource _cancellationTokenSource;
+        private static Task _backgroundTask;
+
+        private static TestLogger _logger;
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = _cancellationTokenSource.Token;
+            _backgroundTask = Task.Run(() => BackgroundService(cancellationToken));
+
+            _logger = new TestLogger(context);  
+        }
+
+        [AssemblyCleanup]
+        public static void Cleanup()
+        {
+            _cancellationTokenSource.Cancel();
+            _backgroundTask.Wait(); 
+            _cancellationTokenSource.Dispose();
+        }
+
+        private static async Task BackgroundService(CancellationToken cancellationToken)
+        {
+       
+            AmsTcpIpRouter router = new AmsTcpIpRouter(new AmsNetId("111.111.111.111.1.1"));
+
+            Task routerTask = router.StartAsync(cancellationToken);
+
+            AdsRouterServer adsRouterService = new AdsRouterServer(router, _logger);
+
+            SystemServiceServer systemService = new SystemServiceServer(router, _logger);
+
+            Task systemServiceTask = systemService.ConnectServerAndWaitAsync(cancellationToken);
+            Task routerServerTask = adsRouterService.ConnectServerAndWaitAsync(cancellationToken);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.WhenAll(routerTask, systemServiceTask, routerServerTask);
+            }
+        }
+    }
+}
